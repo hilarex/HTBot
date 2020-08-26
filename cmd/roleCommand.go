d8652627ff2d3251592d479ab0fd7fc14325fa0f
hilarex <60436889+hilarex@users.noreply.github.com>
2020-08-26 11:08:42 +0200

    add HTB role

--- cmd/roleCommand.go
+++ cmd/roleCommand.go
@@ -10,13 +10,13 @@ import (
 func RoleCommand(ctx framework.Context) {
     
     if len(ctx.Args) == 0{
-        ctx.Reply("Choose a role to add : paris, rennes, tours")
+        ctx.Reply("Choose a role to add : htb, paris, rennes, tours")
         return
     }
     
     roleWanted := strings.ToLower(ctx.Args[0])
     
-    if ! framework.IsInSlice(roleWanted, []string{"paris", "rennes", "tours"}){
+    if ! framework.IsInSlice(roleWanted, []string{"htb", "paris", "rennes", "tours"}){
     	ctx.Reply("I don't know this role..")
     	return
     }
@@ -25,9 +25,15 @@ func RoleCommand(ctx framework.Context) {
     newRole := ""
 
     for _, role := range roles{
-    	if strings.ToLower(ctx.Args[0]) == strings.ToLower(role.Name){
+    	if roleWanted == strings.ToLower(role.Name){
     		newRole = role.ID
+            break
     	}
+
+        if strings.ToLower(role.Name) == "htb player" && roleWanted == "htb"{
+            newRole = role.ID
+            break
+        }
     }
 
     ctx.Discord.GuildMemberRoleAdd(config.Discord.GuildID, ctx.User.ID, newRole)
